bad8ffa6149c83043a50faedff652379a6f9460e
Gooozmen <jguzman.softdev@gmail.com>
2025-04-25 18:39:26 -0300

    Improve code quality of the tokenfactory class

--- src/Infrastructure/Factories/TokenFactory.cs
+++ src/Infrastructure/Factories/TokenFactory.cs
@@ -1,40 +1,50 @@
-using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using Application.Interfaces;
 using Domain.Models;
 using Infrastructure.Options;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.JsonWebTokens;
 using Microsoft.IdentityModel.Tokens;
+using JwtRegisteredClaimNames = System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames;
 
 namespace Infrastructure.Factories;
 
 public class TokenFactory(IOptions<JwtOption> jwtOptions) : ITokenFactory
 {
     private readonly JwtOption _jwtOption = jwtOptions.Value;
-
+    
     public string Create(ApplicationUser user)
     {
-        var claims = new List<Claim>
+        var key = AssemblySecurityKey();
+        var credentials = AssemblySigningCredentials(key);
+        var claims = AssemblyClaims(user);
+        
+        var tokenDescriptor = new SecurityTokenDescriptor
         {
-            new(JwtRegisteredClaimNames.Sub, user.Id.ToString()), // Subject: the user's id.
-            new(JwtRegisteredClaimNames.Name, user.UserName), 
-            new(JwtRegisteredClaimNames.Email, user.Email),
-            new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()) // JWT ID: unique identifier for the token.
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddMinutes(_jwtOption.ExpiryMinutes),
+            SigningCredentials = credentials,
+            Issuer = _jwtOption.Issuer,
+            Audience = _jwtOption.Audience
         };
-
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOption.Key));
-
-        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-
-        var token = new JwtSecurityToken(
-            _jwtOption.Issuer,
-            _jwtOption.Audience,
-            claims,
-            expires: DateTime.Now.AddMinutes(_jwtOption.ExpiryMinutes),
-            signingCredentials: creds
-        );
-        var result = new JwtSecurityTokenHandler().WriteToken(token);
-        return result;
+        
+        var handler = new JsonWebTokenHandler();
+        var token = handler.CreateToken(tokenDescriptor);
+        return token;
     }
+    
+    private SymmetricSecurityKey AssemblySecurityKey() 
+        =>new(Encoding.UTF8.GetBytes(_jwtOption.Key));
+    
+    private SigningCredentials AssemblySigningCredentials(SymmetricSecurityKey key)
+        =>new(key,SecurityAlgorithms.HmacSha256);
+    
+    private IEnumerable<Claim> AssemblyClaims(ApplicationUser user)
+        =>
+        [
+            new(JwtRegisteredClaimNames.Sub, user.Id.ToString()), // Subject: the user's id.
+            new(JwtRegisteredClaimNames.Email, user.Email),
+            new("email_verified", user.EmailConfirmed.ToString())
+        ];
 }
