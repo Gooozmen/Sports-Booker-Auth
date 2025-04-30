bb732d5151381ff1a373d66c68b81c728c14bfca
Gooozmen <jguzman.softdev@gmail.com>
2025-04-30 19:29:53 -0300

    major update: added ci functionality

--- src/Shared/Wrappers/ApplicationUserWrapper.cs
+++ src/Shared/Wrappers/ApplicationUserWrapper.cs
@@ -4,6 +4,6 @@ namespace Shared.Wrappers;
 
 public class ApplicationUserWrapper
 {
-    public ApplicationUser ApplicationUser { get; set; }
+    public required ApplicationUser ApplicationUser { get; set; }
     public string Password { get; set; }
 }
