e525891af9d0dc4ce68a24372f944e36096351b6
Gooozmen <jguzman.softdev@gmail.com>
2025-05-22 17:57:33 -0300

    impove program logging

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -8,64 +8,58 @@ try{
     
     AppContext.SetSwitch("Elastic.Extensions.Logging.EmitDebug", true);
     AppContext.SetSwitch("Elastic.Extensions.Logging.EmitFailure", true);
-
-    Console.WriteLine("Starting up");
     
     var builder = WebApplication.CreateBuilder(args);
         Console.WriteLine("Builder created");
         
     builder.SetupLoggingInfrastructure();
-        Console.WriteLine("Logging infrastructure set up");
-        
-    Console.WriteLine("SINK ACTUAL: " + builder.Configuration["Serilog:WriteTo:0:Name"]);
+    Log.Information("Logging infrastructure set up");
     
     builder.WebHost.UseUrls("http://0.0.0.0:80");
-        Console.WriteLine("URL Defined");
+    Log.Information("URL Defined");
 
     builder.Configuration.AddDefaultConfiguration<Program>();
-        Console.WriteLine("Default configuration added");
+    Log.Information("Default configuration added");
 
     builder.Services.ConfigureOptions(builder.Configuration);
-        Console.WriteLine("Options configured");
+    Log.Information("Options configured");
 
     builder.Services.ConfigureJwt();
-        Console.WriteLine("JWT configured");
+    Log.Information("JWT configured");
 
     builder.Services.AddInfrastructure();
-        Console.WriteLine("Infrastructure services added");
+    Log.Information("Infrastructure services added");
 
     builder.Services.AddApplicationServices();
-        Console.WriteLine("Application services added");
+    Log.Information("Application services added");
 
     builder.Services.AddPresentationServices();
-        Console.WriteLine("Presentation services added");
+    Log.Information("Presentation services added");
 
     var app = builder.Build();
-        Console.WriteLine("Application built");
+    Log.Information("Application built");
 
     app.UsePresentationMiddlewares();
 
     await app.UseEnvironment();
-        Console.WriteLine("Environment set up");
+    Log.Information("Environment set up");
         
     app.UseRouting(); //Configure the middleware pipeline.
-        Console.WriteLine("Routing set up");
+    Log.Information("Routing set up");
 
     app.UseAuthentication();
-        Console.WriteLine("Authentication set up");
+    Log.Information("Authentication set up");
 
     app.UseAuthorization();
-        Console.WriteLine("Authorization set up");
+    Log.Information("Authorization set up");
 
     app.MapControllers(); //Map controllers to endpoints.
-        Console.WriteLine("Controllers mapped to endpoints");
-        
-    Log.Information("Prueba directa a Elastic Cloud desde Auth API");
+    Log.Information("Controllers mapped to endpoints");
         
     await app.RunAsync();
 }
 catch (Exception ex)
 {
-    Console.WriteLine(ex.Message);
+    Log.Error(ex.Message);
     throw;
 }
