8dc8909ffde8f491a110047c3b70133647993d14
Gooozmen <jguzman.softdev@gmail.com>
2025-07-09 01:54:56 -0300

    deploy pipeline

--- src/Infrastructure/Extensions/LoggingExtensions.cs
+++ src/Infrastructure/Extensions/LoggingExtensions.cs
@@ -1,6 +1,7 @@
 using Elastic.Serilog.Sinks;
 using Microsoft.AspNetCore.Builder;
 using Serilog;
+using Serilog.Events;
 
 namespace CourtBooker.Auth.Infrastructure.Extensions;
 
@@ -10,5 +11,27 @@ internal static class LoggingExtensions
     {
         
         builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
+        
+        builder.Host.UseSerilog((context, configuration) =>
+        {
+            // Try grab the first node from configuration:
+            var firstNode = context.Configuration["Serilog:WriteTo:1:Args:nodes:0"];
+            Console.WriteLine($"NODE CHECk: {firstNode}");
+
+            if (string.IsNullOrWhiteSpace(firstNode))
+            {
+                // No valid ES node, so just log to Console
+                configuration
+                    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console();
+            }
+            else
+            {
+                // Node present, let Serilog.Settings.Configuration wire up all sinks
+                configuration
+                    .ReadFrom.Configuration(context.Configuration);
+            }
+        });
     }
 }
